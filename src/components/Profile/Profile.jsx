272f7b76e31a965f699118981b2c40a941b916fa
Hennadii Yarosh <nedinix.tech@gmail.com>
2023-05-27 17:24:59 +0300

    task-3

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,12 @@
-const Profile = ({ username, tag, location, avatar, stats }) => {
+// import PropTypes from 'prop-types';
+
+const Profile = ({
+  username,
+  tag,
+  location,
+  avatar,
+  stats: { views, followers, likes },
+}) => {
   return (
     <div class="profile">
       <div class="description">
@@ -11,19 +19,31 @@ const Profile = ({ username, tag, location, avatar, stats }) => {
       <ul class="stats">
         <li>
           <span class="label">Followers</span>
-          <span class="quantity">{stats.followers}</span>
+          <span class="quantity">{followers}</span>
         </li>
         <li>
           <span class="label">Views</span>
-          <span class="quantity">{stats.views}</span>
+          <span class="quantity">{views}</span>
         </li>
         <li>
           <span class="label">Likes</span>
-          <span class="quantity">{stats.likes}</span>
+          <span class="quantity">{likes}</span>
         </li>
       </ul>
     </div>
   );
 };
+
+// Profile.propTypes = {
+//   username: PropTypes.string,
+//   tag: PropTypes.string,
+//   location: PropTypes.string,
+//   avatar: PropTypes.string,
+//   stats: {
+//     views: PropTypes.number,
+//     followers: PropTypes.number,
+//     likes: PropTypes.number,
+//   },
+// };
 
 export default Profile;
