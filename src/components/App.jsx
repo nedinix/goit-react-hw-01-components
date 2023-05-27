db4aeb2c4b3f5a2e70478da8c7c1c0c785940e81
Hennadii Yarosh <nedinix.tech@gmail.com>
2023-05-27 23:30:04 +0300

    css modules - profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
 import FriendList from './FriendList/FriendList';
 import Transactions from './Transactions/Transactions';
+import Section from './Section/Section';
 import user from '../data/user.json';
 import data from '../data/data.json';
 import friends from '../data/friends.json';
@@ -23,17 +24,27 @@ export const App = () => {
   //   </div>
   // );
   return (
-    <>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />
-      <Transactions items={transactions} />
-    </>
+    <main>
+      <div className="container">
+        <Section title={'Profile'}>
+          <Profile
+            username={user.username}
+            tag={user.tag}
+            location={user.location}
+            avatar={user.avatar}
+            stats={user.stats}
+          />
+        </Section>
+        <Section title={'Statistics'}>
+          <Statistics title="Upload stats" stats={data} />
+        </Section>
+        <Section title={'Friend list'}>
+          <FriendList friends={friends} />
+        </Section>
+        <Section title={'Transactions'}>
+          <Transactions items={transactions} />
+        </Section>
+      </div>
+    </main>
   );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,5 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import css from './Profile.module.css';
 
 export default function Profile({
   username,
@@ -8,40 +9,38 @@ export default function Profile({
   stats: { views, followers, likes },
 }) {
   return (
-    <div class="profile">
-      <div class="description">
-        <img src={avatar} alt={username} class="avatar" />
-        <p class="name">{username}</p>
-        <p class="tag">{tag}</p>
-        <p class="location">{location}</p>
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt={username} className={css.avatar} />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>{tag}</p>
+        <p className={css.location}>{location}</p>
       </div>
 
-      <ul class="stats">
-        <li>
-          <span class="label">Followers</span>
-          <span class="quantity">{followers}</span>
+      <ul className={css.stats}>
+        <li className={css.stats_item}>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{followers}</span>
         </li>
-        <li>
-          <span class="label">Views</span>
-          <span class="quantity">{views}</span>
+        <li className={css.stats_item}>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{views}</span>
         </li>
-        <li>
-          <span class="label">Likes</span>
-          <span class="quantity">{likes}</span>
+        <li className={css.stats_item}>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{likes}</span>
         </li>
       </ul>
     </div>
   );
 }
 
-// Profile.propTypes = {
-//   username: PropTypes.string,
-//   tag: PropTypes.string,
-//   location: PropTypes.string,
-//   avatar: PropTypes.string,
-//   stats: {
-//     views: PropTypes.number,
-//     followers: PropTypes.number,
-//     likes: PropTypes.number,
-//   },
-// };
+Profile.propTypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  views: PropTypes.number,
+  followers: PropTypes.number,
+  likes: PropTypes.number,
+};
