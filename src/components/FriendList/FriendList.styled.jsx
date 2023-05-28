c3ec6478f7472a4de1d2256a8c332968808ed7e3
Hennadii Yarosh <nedinix.tech@gmail.com>
2023-05-28 11:28:46 +0300

    friendList styled

--- src/components/FriendList/FriendList.styled.jsx
+++ src/components/FriendList/FriendList.styled.jsx
@@ -15,8 +15,8 @@ export const StyledFriendListItem = styled.li`
 
   padding-top: 20px;
   padding-bottom: 20px;
-  padding-left: 12px;
-  padding-right: 12px;
+  padding-left: 20px;
+  padding-right: 20px;
 
   font-size: 16px;
   font-weight: 600;
@@ -25,19 +25,42 @@ export const StyledFriendListItem = styled.li`
   background-color: #f4fbfd;
   border-radius: 4px;
 
+  transform: scale(0.99);
   box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
     rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
+
+  transition: background-color 250ms cubic-bezier(0.13, 0.62, 0.66, 0.26),
+    box-shadow 100ms cubic-bezier(0.13, 0.62, 0.66, 0.26),
+    transform 100ms cubic-bezier(0.13, 0.62, 0.66, 0.26);
 
   &:not(:last-child) {
     margin-bottom: 8px;
   }
+
+  &:hover,
+  &:focus {
+    transform: scale(1);
+    box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px,
+      rgba(0, 0, 0, 0.22) 0px 10px 10px;
+  }
 `;
 
 export const StyledFriendStatus = styled.span`
-  width: 12px;
-  height: 12px;
+  width: 18px;
+  height: 18px;
   display: block;
-  border-radius: 50%;
+  margin-right: 14px;
 
-  background-color: green;
+  border-radius: 50%;
+  background-color: ${props => props.color};
+`;
+
+export const StyledFriendAvatar = styled.img`
+  wwidth: 46px;
+  height: 46px;
+  margin-right: 10px;
+
+  border-radius: 8px;
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+    rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
 `;
--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,11 +1,17 @@
-import { StyledFriendListItem, StyledFriendStatus } from './FriendList.styled';
+import {
+  StyledFriendListItem,
+  StyledFriendStatus,
+  StyledFriendAvatar,
+} from './FriendList.styled';
 
 export default function FriendListItem({ avatar, name, isOnline }) {
   return (
     <StyledFriendListItem>
-      <StyledFriendStatus>{isOnline}</StyledFriendStatus>
-      <img class="avatar" src={avatar} alt={name} width="48" />
-      <p class="name">{name}</p>
+      <StyledFriendStatus color={isOnline ? 'green' : 'red'}>
+        {isOnline}
+      </StyledFriendStatus>
+      <StyledFriendAvatar src={avatar} alt={name} width="48" />
+      <p className="name">{name}</p>
     </StyledFriendListItem>
   );
 }
